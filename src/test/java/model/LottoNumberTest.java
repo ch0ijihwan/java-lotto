40cb58b5dba7e696d57ac6fa045d56d3074676b6
cjh8746 <cjh87467@gmail.com>
2022-04-27 22:39:41 +0900

    test : LottoNumber 테스트 코드 작성

---
+++ src/test/java/model/LottoNumberTest.java
@@ -0,0 +1,35 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LottoNumberTest {
+
+    @ParameterizedTest
+    @DisplayName("입력 받은 값이 1 ~ 45 내에 있는 숫자가 아니면 예외처리를 반환한다.")
+    @ValueSource(ints = {0, 46})
+     void create(final int number) {
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoNumber.valueOf(number))
+                .withMessage(String.format("로또 넘버를 생성 할 수 없습니다. 로또 넘버는 %d ~ %d 사이의 숫자여야 합니다.", 1,45));
+    }
+
+    @Test
+    @DisplayName("숫자를 반환한다.")
+    void getNumber() {
+        //given
+        LottoNumber number = LottoNumber.valueOf(1);
+        int expect = 1;
+
+        //when
+        int actual = number.getValue();
+
+        //then
+        assertThat(actual).isEqualTo(expect);
+    }
+}
