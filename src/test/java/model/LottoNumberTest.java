2f13e0e2e712a442df34c7b7c734f89fdfd925d7
cjh8746 <cjh87467@gmail.com>
2022-04-27 23:11:33 +0900

    test : 테스트 코드 수정

--- src/test/java/model/LottoNumberTest.java
+++ src/test/java/model/LottoNumberTest.java
@@ -10,13 +10,16 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 class LottoNumberTest {
 
+    private static final int MIN_LOTTO_NUMBER_VALUE = 1;
+    private static final int MAX_LOTTO_NUMBER_VALUE = 45;
+
     @ParameterizedTest
     @DisplayName("입력 받은 값이 1 ~ 45 내에 있는 숫자가 아니면 예외처리를 반환한다.")
     @ValueSource(ints = {0, 46})
-     void create(final int number) {
+    void create(final int number) {
         //then
         assertThatIllegalArgumentException().isThrownBy(() -> LottoNumber.valueOf(number))
-                .withMessage(String.format("로또 넘버를 생성 할 수 없습니다. 로또 넘버는 %d ~ %d 사이의 숫자여야 합니다.", 1,45));
+                .withMessage(String.format("로또 넘버를 생성 할 수 없습니다. 로또 넘버는 %d ~ %d 사이의 숫자여야 합니다.", MIN_LOTTO_NUMBER_VALUE, MAX_LOTTO_NUMBER_VALUE));
     }
 
     @Test
