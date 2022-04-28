5185d5a6ec6a395638aeee333c20ebfb8a3daf9b
cjh8746 <cjh87467@gmail.com>
2022-04-28 15:41:40 +0900

    test : WinningLotto 테스트 코드 작성

---
+++ src/test/java/model/WinningLottoTest.java
@@ -0,0 +1,41 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinningLottoTest {
+
+    @ParameterizedTest
+    @DisplayName("입력 받은 로또와 당첨 로또를 비교하여 Rank 를 반환한다.")
+    @MethodSource("createMatchingLottoParameterProvider")
+    void measureRank(final Lotto comparisonLotto, final Rank expect) {
+        //given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = Lotto.createLottoNumbers(numbers);
+        LottoNumber bonusBallNumber = LottoNumber.valueOf(7);
+
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusBallNumber);
+
+        //when
+        Rank actual = winningLotto.measureRank(comparisonLotto);
+
+        //then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    static Stream<Arguments> createMatchingLottoParameterProvider() {
+        return Stream.of(
+                Arguments.of(
+                        Lotto.createLottoNumbers(List.of(1, 2, 3, 4, 5, 6)), Rank.FIRST,
+                        Lotto.createLottoNumbers(List.of(1, 2, 3, 4, 5, 7)), Rank.SECOND,
+                        Lotto.createLottoNumbers(List.of(1, 2, 3, 4, 5, 43)), Rank.THIRD)
+        );
+    }
+}
