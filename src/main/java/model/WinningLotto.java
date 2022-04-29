af6d01dd7a961d37afbfc11c8e8f4b243227adb3
cjh8746 <cjh87467@gmail.com>
2022-04-29 10:15:35 +0900

    feat : WinningLotto 구현

---
+++ src/main/java/model/WinningLotto.java
@@ -0,0 +1,25 @@
+package model;
+
+public class WinningLotto {
+
+    private static final int COUNT_TO_HAVE_BONUS = 5;
+    private final Lotto winningLotto;
+    private final LottoNumber bonusBallNumber;
+
+    public WinningLotto(final Lotto winningLotto, final LottoNumber bonusBallNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusBallNumber = bonusBallNumber;
+    }
+
+    public Rank measureRank(final Lotto lotto) {
+        int matchingLottoNumber = lotto.measureMatchingLottoNumber(winningLotto);
+        boolean isBonus = (matchingLottoNumber == COUNT_TO_HAVE_BONUS) && hasBonusLottoNumber(lotto);
+        return Rank.valueOf(matchingLottoNumber, isBonus);
+    }
+
+    private boolean hasBonusLottoNumber(final Lotto lotto) {
+        return lotto.getLottoNumbers()
+                .stream()
+                .anyMatch(lottoNumber -> lottoNumber.equals(bonusBallNumber));
+    }
+}
