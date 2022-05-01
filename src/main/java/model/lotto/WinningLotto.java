cbfd30a2bde31c252716265304bb767df3389687
cjh8746 <cjh87467@gmail.com>
2022-05-01 18:00:17 +0900

    refactor : 로또 넘버와 보너스 넘버 중복 예외처리 로직 추가

--- src/main/java/model/lotto/WinningLotto.java
+++ src/main/java/model/lotto/WinningLotto.java
@@ -10,8 +10,21 @@ public class WinningLotto {
     private final LottoNumber bonusBallNumber;
 
     public WinningLotto(final Lotto winningLotto, final LottoNumber bonusBallNumber) {
+        validateDuplication(winningLotto, bonusBallNumber);
         this.winningLotto = winningLotto;
         this.bonusBallNumber = bonusBallNumber;
+    }
+
+    private void validateDuplication(final Lotto winningLotto, final LottoNumber bonusBallNumber) {
+        if (hasSameLottoNumber(winningLotto, bonusBallNumber)) {
+            throw new IllegalArgumentException("보너스 넘버와 당첨 넘버 중 중복이 있습니다.");
+        }
+    }
+
+    private boolean hasSameLottoNumber(final Lotto winningLotto, final LottoNumber bonusBallNumber) {
+        return winningLotto.getLottoNumbers()
+                .stream()
+                .anyMatch(lottoNumber -> lottoNumber.equals(bonusBallNumber));
     }
 
     public Rank measureRank(final Lotto lotto) {
