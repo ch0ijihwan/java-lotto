5d6fa9e3893e47ca4aca27f8b0b575dd9cd09dd4
cjh8746 <cjh87467@gmail.com>
2022-05-05 16:55:40 +0900

    feat : Profit 구현

---
+++ src/main/java/model/vo/Profit.java
@@ -0,0 +1,27 @@
+package model.vo;
+
+import model.Money;
+
+import java.util.Map;
+
+public class Profit {
+
+    private final double profit;
+    private final Money initialMoney;
+
+    public Profit(final Money initialMoney, final Map<Rank, Integer> lottoGameResult) {
+        profit = calculateProfit(lottoGameResult);
+        this.initialMoney = initialMoney;
+    }
+
+    private double calculateProfit(final Map<Rank, Integer> resultTotalRanks) {
+        return resultTotalRanks.keySet()
+                .stream()
+                .mapToInt(targetRank -> targetRank.getDividend() * resultTotalRanks.get(targetRank))
+                .sum();
+    }
+
+    public double getRateOfProfit() {
+        return profit / initialMoney.getMoneyValue();
+    }
+}
