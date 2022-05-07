0b12c359bd4c6ee54e032ce02c1e095681d27848
cjh8746 <cjh87467@gmail.com>
2022-05-07 17:01:16 +0900

    test : 테스트 코드 생성

--- src/test/java/model/LottoTicketTest.java
+++ src/test/java/model/LottoTicketTest.java
@@ -1,8 +1,6 @@
 package model;
 
 import model.vo.Lotto;
-import model.vo.Money;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,55 +10,20 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTicketTest {
 
-    private LottoTicket lottoTicket;
-
-    @BeforeEach
-    void setUp() {
-        Money money = new Money(10000);
-        int numberOfManualLotto = 3;
-        List<List<Integer>> manualLottoNumbers = List.of(
-                List.of(1, 2, 3, 4, 5, 6),
-                List.of(7, 8, 9, 10, 11, 12)
-        );
-        lottoTicket = new LottoTicket(money, numberOfManualLotto, manualLottoNumbers);
-    }
-
     @Test
-    @DisplayName("자동 로또 갯수를 반환한다.")
-    void getNumberOfAutoLotto() {
+    @DisplayName("입력 받은 수동 로또와 자동 로또 갯수로만든 lottos 를 반환한다.")
+    void createLottos() {
         //given
-        int expect = 7;
-
-        //when
-        int actual = lottoTicket.getNumberOfAutoLotto();
-
-        //then
-        assertThat(actual).isEqualTo(expect);
-    }
-
-    @Test
-    @DisplayName("수동 로또 갯수를 반환한다.")
-    void getNumberOfManualLotto() {
-        //given
-        int expect = 3;
-
-        //when
-        int actual = lottoTicket.getNumberOfManualLotto();
-
-        //then
-        assertThat(actual).isEqualTo(expect);
-    }
-
-    @Test
-    @DisplayName("수동으로 구매할 번호들을 입력 받아 수동 로또 번호들을 반환한다.")
-    void createManualLottos() {
-        //given
-        List<Lotto> expect = List.of(Lotto.createManualLottoNumbers(List.of(1, 2, 3, 4, 5, 6)),
+        int countOfAutoLotto = 11;
+        List<Lotto> manualLotto = List.of(Lotto.createManualLottoNumbers(List.of(1, 2, 3, 4, 5, 6)),
                 Lotto.createManualLottoNumbers(List.of(7, 8, 9, 10, 11, 12)));
+
+        LottoTicket lottoTicket = new LottoTicket(countOfAutoLotto, manualLotto);
+
         //when
-        List<Lotto> actual = lottoTicket.getManualLottos();
+        Lottos actual = lottoTicket.getLottos();
 
         //then
-        assertThat(actual).isEqualTo(expect);
+        assertThat(actual.getLottos().size()).isEqualTo(13);
     }
 }
