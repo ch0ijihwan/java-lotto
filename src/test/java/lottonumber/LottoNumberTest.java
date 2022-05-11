496515824d3b7ef09a0ab9788140b3fb705e1275
cjh8746 <cjh87467@gmail.com>
2022-05-12 00:20:32 +0900

    test : LottoNumber 테스트 구현

---
+++ src/test/java/lottonumber/LottoNumberTest.java
@@ -0,0 +1,34 @@
+package lottonumber;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LottoNumberTest {
+    @ParameterizedTest
+    @DisplayName("로또 넘버는 1에서 45 까지어야 합니다.")
+    @ValueSource(ints = {0, 46})
+    void validateRange(final int input) {
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoNumber.valueOf(input))
+                .withMessage("로또 넘버는 1에서 45 까지어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("로또 넘버 값을 반환한다.")
+    void getValue() {
+        //given
+        int input = 1;
+        LottoNumber lottoNumber = LottoNumber.valueOf(input);
+
+        //when
+        int actual = lottoNumber.getValue();
+
+        //then
+        assertThat(actual).isEqualTo(input);
+    }
+}
