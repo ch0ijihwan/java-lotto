c11557a236cea770b44a08ea516a263c45429d71
cjh8746 <cjh87467@gmail.com>
2022-05-12 15:14:27 +0900

    feat : Lotto 구현

---
+++ src/main/java/model/Lotto.java
@@ -0,0 +1,56 @@
+package model;
+
+import vo.LottoNumber;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
+
+    private final List<LottoNumber> lottoNumbers;
+
+    public Lotto(final List<LottoNumber> lottoNumbers) {
+        validateSize(lottoNumbers);
+        validateDuplication(lottoNumbers);
+        this.lottoNumbers = sortLottoNumbers(lottoNumbers);
+    }
+
+    private List<LottoNumber> sortLottoNumbers(final List<LottoNumber> lottoNumbers) {
+        return lottoNumbers.stream()
+                .sorted()
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void validateDuplication(final List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 안의 로또 넘버는 6가지어야 합니다.");
+        }
+    }
+
+    private void validateSize(final List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.stream().distinct().count() != lottoNumbers.size()) {
+            throw new IllegalArgumentException("로또안의 로또 넘버는 중복이 될 수 없습니다.");
+        }
+    }
+
+    public List<LottoNumber> getLottoNumbers() {
+        return Collections.unmodifiableList(lottoNumbers);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
+    }
+}
