166dee11226def3ad099aa09b451f577736dcb80
cjh8746 <cjh87467@gmail.com>
2022-05-12 17:25:48 +0900

    test : 테스트 코드 작성

---
+++ src/test/java/model/WinningLottoTest.java
@@ -0,0 +1,58 @@
+package model;
+
+import model.factory.LottoFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import vo.Lotto;
+import vo.LottoNumber;
+import vo.Rank;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class WinningLottoTest {
+
+    @Test
+    @DisplayName("보너스 넘버와 당첨 로또 넘버는 중복 될 수 없습니다.")
+    void validateDuplicationWithBonusNumber() {
+        //given
+        Lotto winningLottoInput = LottoFactory.createManualLotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoNumber bonusNumber = LottoNumber.valueOf(6);
+
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() -> new WinningLotto(winningLottoInput, bonusNumber))
+                .withMessage("보너스 넘버와 당첨 로또 넘버는 중복 될 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또 넘버를 받아, 당첨 로또와 비교하여 해당 등수를 반환한다.")
+    @MethodSource("createMatchingLottoParameterProvider")
+    void judgeRank(final Lotto lottoToCompare, final Rank expect) {
+        //given
+        Lotto winningLottoInput = LottoFactory.createManualLotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoNumber bonusNumber = LottoNumber.valueOf(7);
+        WinningLotto winningLotto = new WinningLotto(winningLottoInput, bonusNumber);
+
+        //when
+        Rank actual = winningLotto.judgeRank(lottoToCompare);
+
+        //then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    static Stream<Arguments> createMatchingLottoParameterProvider() {
+        return Stream.of(
+                Arguments.of(
+                        LottoFactory.createManualLotto(List.of(1, 2, 3, 4, 5, 6)), Rank.FIRST,
+                        LottoFactory.createManualLotto(List.of(1, 2, 3, 4, 5, 7)), Rank.SECOND,
+                        LottoFactory.createManualLotto(List.of(1, 2, 3, 4, 5, 45)), Rank.THIRD,
+                        LottoFactory.createManualLotto(List.of(1, 2, 3, 4, 44, 45)), Rank.FOURTH)
+        );
+    }
+}
