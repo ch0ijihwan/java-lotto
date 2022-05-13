0363077c53d25570c5aa4334b09f1c39752a192b
cjh8746 <cjh87467@gmail.com>
2022-05-13 15:29:31 +0900

    feat : TotalPurchaseAmount 구현

---
+++ src/main/java/model/TotalPurchaseAmount.java
@@ -0,0 +1,36 @@
+package model;
+
+public class TotalPurchaseAmount {
+
+    private static final int MIN_TOTAL_PURCHASE_AMOUNT = 0;
+
+    private final int totalPurchaseAmount;
+    private final int lottoPrice;
+
+    public TotalPurchaseAmount(final int totalPurchaseAmount, final int lottoPrice) {
+        validateRange(totalPurchaseAmount);
+        validateMultipleOfLottoPrice(totalPurchaseAmount, lottoPrice);
+        this.totalPurchaseAmount = totalPurchaseAmount;
+        this.lottoPrice = lottoPrice;
+    }
+
+    private void validateRange(final int totalPurchaseAmount) {
+        if (totalPurchaseAmount <= MIN_TOTAL_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("로또를 구매하려면 0 초과의 금액을 입력해야합니다.");
+        }
+    }
+
+    private void validateMultipleOfLottoPrice(final int totalPurchaseAmount, final int lottoPrice) {
+        if (totalPurchaseAmount % lottoPrice != 0) {
+            throw new IllegalArgumentException("로또 가격의 배수인 금액을 넣어야 로또를 구매 할 수 있습니다.");
+        }
+    }
+
+    public int getCountOfTotalPurchase() {
+        return totalPurchaseAmount / lottoPrice;
+    }
+
+    public int getValue() {
+        return totalPurchaseAmount;
+    }
+}
