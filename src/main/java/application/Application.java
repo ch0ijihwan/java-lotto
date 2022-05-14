c2ea985a46d5b7e8a88e60d40da18cb2cb0ac15d
cjh8746 <cjh87467@gmail.com>
2022-05-14 11:19:10 +0900

    feat : 앱실행을 위한 Application 생성

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,16 @@
+package application;
+
+import controller.Controller;
+import view.display.ConsoleDisplay;
+import view.display.Display;
+import view.input.ConsoleInput;
+import view.input.Input;
+
+public class Application {
+    public static void main(String[] args) {
+        Input input = new ConsoleInput();
+        Display display = new ConsoleDisplay();
+        Controller controller = new Controller(input, display);
+        controller.run();
+    }
+}
