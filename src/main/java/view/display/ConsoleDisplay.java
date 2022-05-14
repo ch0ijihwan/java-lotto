4d6402a0a8a2391927257363b614515fa266ea93
cjh8746 <cjh87467@gmail.com>
2022-05-14 10:44:15 +0900

    refactor : view 수정

--- src/main/java/view/display/ConsoleDisplay.java
+++ src/main/java/view/display/ConsoleDisplay.java
@@ -9,13 +9,13 @@ import java.util.Map;
 public class ConsoleDisplay implements Display {
 
     private static final int NON_MATCHED = 0;
-    private static final String COUNT_OF_LOTTO_MESSAGE = "자동 로또 %d개, 수동 로또 %d개를 구매하였습니다.";
-    private static final String RESULT_OF_MATCHING_LOTTO_MESSAGE = "%d 개 일치 %s ( %d 원) - %d";
-    private static final String RATE_OF_PROFIT_MESSAGE = "총 수익률은 %f 입니다.";
+    private static final int SECOND_THRESHOLD = 0;
+    private static final String COUNT_OF_LOTTO_MESSAGE = "자동 로또 %d개, 수동 로또 %d개를 구매하였습니다.\n && ";
+    private static final String RESULT_OF_MATCHING_LOTTO_MESSAGE = "%d 개 일치 %s ( %d 원) - %d \n";
+    private static final String RATE_OF_PROFIT_MESSAGE = "총 수익률은 %f 입니다.\n";
     private static final String BONUS_MATCHING_MESSAGE = ", 보너스 일치";
     private static final String WINNING_STATISTICS = "당첨 통계";
     private static final String MULTI_BARS = "----------";
-    private static final String LOTTO_FORMAT = "[%s]\n";
 
     @Override
     public void displayPurchaseCount(final int countOfAutoPurchase, final int countOfManualPurchase) {
@@ -23,9 +23,8 @@ public class ConsoleDisplay implements Display {
     }
 
     @Override
-    public void displayLotteryTicket(List<LottoDto> lottos) {
-        System.out.printf(COUNT_OF_LOTTO_MESSAGE, lottos.size());
-        lottos.forEach(lotto -> System.out.printf(LOTTO_FORMAT, lotto));
+    public void displayLotteryTicket(final List<LottoDto> lottos) {
+        lottos.forEach(lotto -> System.out.println(lotto.getLottoNumbers()));
     }
 
     @Override
@@ -40,7 +39,7 @@ public class ConsoleDisplay implements Display {
 
     private void displayLottoResult(final Rank rank, final int matchedCount) {
         String bonusMessage = " ";
-        if (rank == Rank.SECOND) {
+        if (rank == Rank.SECOND && matchedCount == SECOND_THRESHOLD) {
             bonusMessage = BONUS_MATCHING_MESSAGE;
         }
         System.out.printf(RESULT_OF_MATCHING_LOTTO_MESSAGE, rank.getMatchingCount(), bonusMessage, rank.getReward(), matchedCount);
--- src/main/java/view/input/ConsoleInput.java
+++ src/main/java/view/input/ConsoleInput.java
@@ -2,22 +2,21 @@ package view.input;
 
 import model.dto.LottoDto;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ConsoleInput implements Input {
 
-    private static final Scanner SCANNER = new Scanner(System.in);
     private static final String LOTTO_NUMBER_DELIMITER = ",";
     private static final String INPUT_TOTAL_PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼 넘버를 입력해주세요. ";
     private static final String INPUT_COUNT_OF_MANUAL_PURCHASE_MESSAGE = "수동으로 구매할 로또 수를 입력해주세요.";
     private static final String INPUT_MANUAL_LOTTO_NUMBERS_MESSAGE = "수동으로 구매할 번호를 입력해 주세요";
     private static final String INPUT_WINNING_LOTTO_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
-
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     @Override
     public int inputTotalPurchaseAmount() {
@@ -35,27 +34,37 @@ public class ConsoleInput implements Input {
     @Override
     public List<LottoDto> inputManualLottoNumbers(final int countOfManualPurchase) {
         System.out.println(INPUT_MANUAL_LOTTO_NUMBERS_MESSAGE);
-        return Stream.generate(this::inputLotteryNumbers)
-                .limit(countOfManualPurchase)
+        List<String> informationNumbers = new ArrayList<>();
+        for (int i = 0; i < countOfManualPurchase; i++) {
+            SCANNER.nextLine();
+            informationNumbers.add(SCANNER.nextLine());
+        }
+        return convertToLottoDto(informationNumbers);
+    }
+
+    private List<LottoDto> convertToLottoDto(final List<String> informationNumbers) {
+        return informationNumbers.stream()
+                .map(this::convertToSplitInteger)
                 .map(LottoDto::new)
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    private List<Integer> inputLotteryNumbers() {
-        return Arrays.stream(splitLotteryNumbers())
+    private List<Integer> convertToSplitInteger(final String numbers) {
+        return Arrays.stream(splitNumbers(numbers))
                 .map(Integer::parseInt)
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    private String[] splitLotteryNumbers() {
-        return SCANNER.nextLine()
-                .split(ConsoleInput.LOTTO_NUMBER_DELIMITER);
+    private String[] splitNumbers(final String numbers) {
+        return numbers.strip()
+                .split(LOTTO_NUMBER_DELIMITER);
     }
+
 
     @Override
     public List<Integer> inputWiningLotto() {
         System.out.println(INPUT_WINNING_LOTTO_NUMBERS);
-        return inputLotteryNumbers();
+        return convertToSplitInteger(SCANNER.nextLine());
     }
 
     @Override
